cd08394a90ab44aa131dcc9fecd98f6661656263
David Miguel <tuemail@ejemplo.com>
2026-09-17 09:17:35 +0200

    .

---
+++ lib/OnBoardingView.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Onboardingview extends StatefulWidget{
+  const Onboardingview({super.key});
+
+  @override
+  State<Onboardingview> createState() => _Onboardingview();
+}
+
+class _Onboardingview extends State<Onboardingview>{
+  @override
+  Widget build(BuildContext context){
+    return Container(color: const Color((0xFFFFE306)));
+  }
+}
